786ca95ccb1022f0c33e413e4e4443137b1d8306
IrinaOleynik <irinaoleynik615@gmail.com>
2024-04-25 17:20:20 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class PhoneBook {
     static Map<String, String> phoneBook = new HashMap<>();
     public static int add(String name, String phoneNumber) {
-       return 0;
+       phoneBook.put(name, phoneNumber);
+       return phoneBook.size();
     }
 }
