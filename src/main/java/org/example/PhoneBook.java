5177c71c6cd7d0c2d6e3005907b4a69e6c42c087
IrinaOleynik <irinaoleynik615@gmail.com>
2024-04-25 17:18:03 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    static Map<String, String> phoneBook = new HashMap<>();
+    public static int add(String name, String phoneNumber) {
+       return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,26 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
 
 class PhoneBookTest {
 
+    public static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of("Мария", "89900324534", 1),
+                Arguments.of("Иван", "89880356534", 2),
+                Arguments.of("Василий", "89367410845", 3),
+                Arguments.of("Мария", "89250759178", 3)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void add(String name, String phoneNumber, int expected) {
+        int result = PhoneBook.add(name, phoneNumber);
+        Assertions.assertEquals(expected, result);
+    }
 }
